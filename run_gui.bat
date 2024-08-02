6f9290e8f8fa31eabd408b0a315fc18a6d88a310
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-08-02 14:10:34 +0300

    .

--- run_gui.bat
+++ run_gui.bat
@@ -1,1 +1,23 @@
-java --enable-preview -jar target/com.tugalsan.dsk.file.sync-jar-with-dependencies_cleaned-and-migrated.jar
+@echo off
+:: BatchGotAdmin (Run as Admin code starts)
+REM --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+echo Requesting administrative privileges...
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
+CD /D "%~dp0"
+:: BatchGotAdmin (Run as Admin code ends)
+:: Your codes should start from the following line
+
+echo hello as admin
+java --enable-preview -jar C:\git\dsk\com.tugalsan.dsk.file.sync\target\com.tugalsan.dsk.file.sync-jar-with-dependencies_cleaned-and-migrated.jar
